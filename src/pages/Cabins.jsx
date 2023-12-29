7e0f4dff11cd4d8d2a04c2394a61a76d82ea3110
Pranav Aher <prranavyaher@gmail.com>
2023-12-29 23:29:26 +0530

    Add create cabin functionality.

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,8 +2,13 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import { useState } from "react";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Row type="horizontal">
@@ -13,6 +18,10 @@ function Cabins() {
       <Row>
         <CabinTable />
       </Row>
+      <Button onClick={() => setShowForm((show) => !show)}>
+        Add new cabin
+      </Button>
+      {showForm && <CreateCabinForm />}
     </>
   );
 }
